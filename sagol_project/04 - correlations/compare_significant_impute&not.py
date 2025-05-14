5d3f620d436bf144285af61e37d7cb98c314ea09
Halel Adorian <haleladorian@mail.tau.ac.il>
2025-05-14 22:05:46 +0300

    check significant after impute data

---
+++ sagol_project/04 - correlations/compare_significant_impute&not.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# טען את שני הקבצים
+df1 = pd.read_csv("./results/all_significant_correlations.csv")
+df2 = pd.read_csv("./results_imuted_data/all_significant_correlations_im.csv")
+
+# קח רק את העמודות הרלוונטיות
+pairs1 = df1[['Feature_1', 'Feature_2']]
+pairs2 = df2[['Feature_1', 'Feature_2']]
+
+# ודא שאין כפילויות
+pairs1 = pairs1.drop_duplicates().reset_index(drop=True)
+pairs2 = pairs2.drop_duplicates().reset_index(drop=True)
+
+# בדוק אילו זוגות מהקובץ הראשון לא נמצאים בשני
+missing_pairs = pairs1[~pairs1.apply(tuple, axis=1).isin(pairs2.apply(tuple, axis=1))]
+
+# הצג את הזוגות החסרים
+print("Missing pairs from file2:")
+print(missing_pairs)
